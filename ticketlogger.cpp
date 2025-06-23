46a63089e4ece1f1c8e7bf197cc7b7872c499fd5
blueberrypie167 <bfgeneral92@gmail.com>
2025-06-23 05:06:53 +0300

    ticket logger

---
+++ ticketlogger.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+/*  this program will collect annotated details about customer's tickets, 
+    including the time, date, contact details and the content of the problem
+    then it will store these files in a text file, eventually in a database
+    mostly oop for now i will figure what to implement using data structures later
+*/
+
+
+int main(){
+    
+
+}
